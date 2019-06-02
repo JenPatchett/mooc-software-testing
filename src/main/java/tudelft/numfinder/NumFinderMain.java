6ca7b3502887ade5372854b6e9a36fbd38575f48
Jen Patchett <jennifer.patchett@sky.uk>
2019-06-02 16:42:48 +0100

    Fix bug: find largest and smallest numbers in array

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -5,11 +5,7 @@ public class NumFinderMain {
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
-
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new Integer[] {79, 9, 4, 17, 22, 0, -43});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
